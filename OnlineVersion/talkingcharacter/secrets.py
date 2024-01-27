ea8b370247a16e62c412f91ece897fda21db04af
Eliasl3l <59790316+Eliasl3l@users.noreply.github.com>
2024-01-27 02:34:58 +0100

    Update secrets.py

--- OnlineVersion/talkingcharacter/secrets.py
+++ OnlineVersion/talkingcharacter/secrets.py
@@ -1,1 +1,12 @@
-
+SECRET_KEY = ""
+OPENAI_KEY=''
+TOKEN_DID=''
+USERNAME_DID=''
+PASSWORD_DID=''
+NGROK_KEY='https://...ngrok.io'
+NGROK_AUTH_TOKEN = ""
+SDK_CLOUD_NAME = "dntslmaln" 
+SDK_API_KEY = "numbers" 
+SDK_API_SECRET = "lettersAndNumbers"
+#the secrets are stored here so the arent published with the publication of the source code
+# environment variables are better tho, and should be used next time
